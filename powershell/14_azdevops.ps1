df1650e913c997e858fe7e9258356ef0c89dd392
daniheck-msft <daniheck@microsoft.com>
2023-05-08 18:53:17 +0200

    Remove ignored files

--- powershell/14_azdevops.ps1
+++
@@ -1,9 +0,0 @@
-# creating an Azure DevOps organization through API is not supported yet
-
-# Documentation: https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/create-organization?view=azure-devops&tabs=preview-page
-# Pricing: https://azure.microsoft.com/en-us/pricing/details/devops/azure-devops-services/
-
-# install az cli extension for azure devops
-az extension add --name azure-devops
-
-
